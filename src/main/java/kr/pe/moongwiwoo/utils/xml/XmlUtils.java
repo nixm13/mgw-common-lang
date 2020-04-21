9cf721c195a890ad8b142af82eeea0dd60ac77e3
Moon Gwi Woo <nixm13@naver.com>
2020-04-21 19:46:45 +0900

    - 버전업 없이 수정
    - JAXBContext Moxy 모듈 적용
    - 메소드 마샬 옵션 세분화

--- src/main/java/kr/pe/moongwiwoo/utils/xml/XmlUtils.java
+++ src/main/java/kr/pe/moongwiwoo/utils/xml/XmlUtils.java
@@ -8,6 +8,8 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import org.eclipse.persistence.jaxb.JAXBContextFactory;
+
 
 /**
  * <pre>
@@ -24,16 +26,39 @@ import javax.xml.bind.Unmarshaller;
 public class XmlUtils {
 
 	/**
+	 * JAXB
 	 * 객체를 XMLString 으로 반환
+	 * JAXB_FORMATTED_OUTPUT true
+	 * JAXB_FRAGMENT false
 	 * @return String
 	 */
 	public static <T> String parserObjToStr(T obj) throws JAXBException {
+		return parserObjToStr(obj, true);
+	}
 
-		JAXBContext jaxbContext = JAXBContext.newInstance(obj.getClass());
+	/**
+	 * JAXB
+	 * 객체를 XMLString 으로 반환
+	 * JAXB_FRAGMENT false
+	 * @return String
+	 */
+	public static <T> String parserObjToStr(T obj, boolean isFormat) throws JAXBException {
+		return parserObjToStr(obj, isFormat, false);
+	}
+
+	/**
+	 * JAXB
+	 * 객체를 XMLString 으로 반환
+	 * @return String
+	 */
+	public static <T> String parserObjToStr(T obj, boolean isFormat, boolean isFragment) throws JAXBException {
+
+		JAXBContext jaxbContext = JAXBContextFactory.createContext(new Class[] { obj.getClass() }, null);
 	    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 	    StringWriter sw = new StringWriter();
 
-	    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+	    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, isFormat);
+	    jaxbMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, isFragment);
 	    jaxbMarshaller.marshal(obj, sw);
 
 		return sw.toString();
@@ -46,7 +71,7 @@ public class XmlUtils {
 	 */
 	public static <T> T parserStrToObj(String xml, Class<T> clazz) throws JAXBException {
 
-		JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+		JAXBContext jaxbContext = JAXBContextFactory.createContext(new Class[] { clazz }, null);
 		Unmarshaller jaxUnmarshaller = jaxbContext.createUnmarshaller();
 
 		Object obj = jaxUnmarshaller.unmarshal(new StringReader(xml));
